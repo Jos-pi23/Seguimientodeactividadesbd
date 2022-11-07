3f78f2b40ab5344458a6dc06782899aefbffe7e9
JoelOrrala <91575914+JoelOrrala@users.noreply.github.com>
2022-11-06 22:44:21 -0500

    Update menu_opciones.py

--- menu_opciones.py
+++ menu_opciones.py
@@ -4,10 +4,10 @@ print("1. Ingresar a cuenta")
 print("2. Crear cuenta")
 ingreOP = input("Ingrese una opción: ")
 if ingreOP.isdigit():
-    if ingreOP == 1:
+    if ingreOP == "1":
         ingreUser = input("Ingrese su email: ")
         ingrePass = input("Ingrese su password: ")
-    elif ingreOP == 2:
+    elif ingreOP == "2":
         newNombre = input("Ingrese su nombre: ")
         newMail = input("Ingrese un email: ")
         newPass = input("Ingrese una contraseña: ")
