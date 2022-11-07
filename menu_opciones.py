e5e1dc160b7c3e841e292bf7fae3967ae880ef0f
JoelOrrala <91575914+JoelOrrala@users.noreply.github.com>
2022-11-06 21:43:57 -0500

    Update menu_opciones.py

--- menu_opciones.py
+++ menu_opciones.py
@@ -2,14 +2,16 @@ print("Menu de opciones")
 print("Bienvenid@")
 print("1. Ingresar a cuenta")
 print("2. Crear cuenta")
-ingreOP = int(input("Ingrese una opción: "))
-if ingreOP == 1:
-    ingreUser = input("Ingrese su email: ")
-    ingrePass = input("Ingrese su password: ")
-elif ingreOP == 2:
-    newNombre = input("Ingrese su nombre: ")
-    newMail = input("Ingrese un email: ")
-    newPass = input("Ingrese una contraseña: ")
-    print("Cuenta registrada")
-else:
-    print("Opción no válida")
+ingreOP = input("Ingrese una opción: ")
+if ingreOP.isdigit():
+    if ingreOP == 1:
+        ingreUser = input("Ingrese su email: ")
+        ingrePass = input("Ingrese su password: ")
+    elif ingreOP == 2:
+        newNombre = input("Ingrese su nombre: ")
+        newMail = input("Ingrese un email: ")
+        newPass = input("Ingrese una contraseña: ")
+        print("Cuenta registrada")
+        print("¿Desea regresar? si/no")
+    else:
+        print("Opción no válida")
