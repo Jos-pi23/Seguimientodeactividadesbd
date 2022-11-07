8c39b8c054076652ea29a9c953bfd543bef58a72
JoelOrrala <91575914+JoelOrrala@users.noreply.github.com>
2022-11-06 21:31:42 -0500

    Update menu_opciones.py

--- menu_opciones.py
+++ menu_opciones.py
@@ -1,1 +1,15 @@
 print("Menu de opciones")
+print("Bienvenid@")
+print("1. Ingresar a cuenta")
+print("2. Crear cuenta")
+ingreOP = int(input("Ingrese una opción: "))
+if ingreOP == 1:
+    ingreUser = input("Ingrese su email: ")
+    ingrePass = input("Ingrese su password: ")
+elif ingreOP == 2:
+    newNombre = input("Ingrese su nombre: ")
+    newNombre = input("Ingrese un email: ")
+    newNombre = input("Ingrese una contraseña: ")
+    print("Cuenta registrada")
+else:
+    print("Opción no válida")
