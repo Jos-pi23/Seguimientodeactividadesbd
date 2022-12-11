9fdc89282c23ad8264d0176987a7ca2b08dcab1e
JoelOrrala <91575914+JoelOrrala@users.noreply.github.com>
2022-12-10 22:12:44 -0500

    Update creacionDetablas.sql

--- creacionDetablas.sql
+++ creacionDetablas.sql
@@ -32,3 +32,38 @@ create table Tarea(
   calificacion int default 0 Check(calificacion<11 and calificacion>-1),
   estado enum('pendiente', 'realizado') not null
 );
+
+CREATE TABLE Usuario (
+	id_user INT AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    mail VARCHAR(50) NOT NULL,
+    contrasenia VARCHAR(20) NOT NULL,
+    esLider BOOLEAN NOT NULL,
+    esColaborador BOOLEAN NOT NULL,
+    PRIMARY KEY(id_user)
+);
+
+CREATE TABLE Proyecto (
+	id_proyecto INT AUTO_INCREMENT,
+    fecha_creacion DATE NOT NULL,
+    fecha_cierre DATE NOT NULL,
+    hora_cierre TIME NOT NULL,
+    estado ENUM('pendiente','realizado') NOT NULL,
+	descripcion  VARCHAR(200) NOT NULL,
+    lider INT NOT NULL,
+    PRIMARY KEY(id_proyecto),
+    FOREIGN KEY(lider) REFERENCES Lider(id_lider)
+);
+
+CREATE TABLE Registro_ampliacion (
+	id_resAmpliacion INT AUTO_INCREMENT,
+    estado ENUM('pendiente','aceptada') NOT NULL,
+    fecha DATE NOT NULL,
+	descripcion  VARCHAR(100) NOT NULL,
+    colaborador INT NOT NULL,
+    lider INT NOT NULL,
+    tarea INT NOT NULL,
+    PRIMARY KEY(id_proyecto),
+    FOREIGN KEY(lider) REFERENCES Lider(id_lider),
+    FOREIGN KEY(tarea) REFERENCES Tarea(id_tarea)
+);
